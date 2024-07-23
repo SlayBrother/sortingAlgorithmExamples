c2db25af9c4e84fd2b841167b45b4ed7ae9f5d92
SlayBrother <cjcfa1@gmail.com>
2024-07-22 21:49:12 -0500

    first commit

---
+++ bubble.js
@@ -0,0 +1,19 @@
+function bubbleSort() {let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let swapped = false;
+        for (let j = 0; j < arr.length - i - 1; j++) { // corrected loop condition
+            count++;
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    }
+    console.log("Total Count:", count);
+    return arr; // return the sorted array
+}
+
+module.exports = bubbleSort;
